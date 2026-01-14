ede6e916dd090a88cf526f7a8cdb49a7654f0abc
Luis francisco Sanchez turrion <luisfranciscosanchezturrion@MacBook-Air-de-Luis.local>
2026-01-14 17:58:08 +0100

    GP2Y1010

--- GP2Y1010.c
+++ GP2Y1010.c
@@ -17,23 +17,16 @@ int GP2Y1010_ReadRaw(uint8_t channel)
 	uint32_t t_off;
 	int adc;
 
-	// DEBUG: encendiendo LED.
 	GPOUT |= M2_ON_OFF;
-	// DEBUG: esperando a que el contador llegue al punto de muestreo.
 	while ((uint32_t)(TIMER - start) < sample_ticks) {
 	}
-	// DEBUG: contador pasado, tomar ADC.
 	t_sample = TIMER;
 	adc = MCP3004_Read(channel);
-	// DEBUG: manteniendo LED encendido hasta completar la ventana.
 	while ((uint32_t)(TIMER - start) < total_ticks) {
 	}
-	// DEBUG: apagando LED.
 	t_off = TIMER;
-	GPOUT &= ~M2_ON_OFF;
-	_printf("GP2Y1010: LED ON t=%u\n", start);
+
 	_printf("GP2Y1010: sample t=%u adc=%d\n", t_sample, adc);
-	_printf("GP2Y1010: LED OFF t=%u\n", t_off);
 
 	return adc;
 }
--- GP2Y1010.h
+++ GP2Y1010.h
@@ -2,7 +2,6 @@
 
 #include <stdint.h>
 
-// Tiempos en microsegundos (segun libreria de Arduino).
 #define GP2Y1010_LED_ON_US 320u
 #define GP2Y1010_SAMPLE_US 280u
 
